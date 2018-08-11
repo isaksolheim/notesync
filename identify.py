2c1107e752eaf53b95654415b4a5e775ae84d65d
aqpwsec <nogolfpls@protonmail.com>
2018-08-11 18:28:34 +0000

    first fully working product

---
+++ identify.py
@@ -0,0 +1,66 @@
+from PIL import Image
+import sys
+
+def classify(image):
+    img = Image.open(image)
+
+    #resize to smaller image
+    w,h = img.size
+    img.resize((w//10,h//10)).save("tmp.jpg")
+    img.close()
+
+    img = Image.open("tmp.jpg")
+    pix = img.load()
+
+    w,h = img.size
+
+
+    blue = [range(60,90),range(110,140),range(110,140)]
+    red = [range(180,230), range(70,115), range(30,70)]
+    green = [range(130,170),range(140,180), range(60,85)]
+    yellow = [range(200,235), range(180,210), range(95,120)]
+    pink = [range(210,240), range(150,180), range(110,130)]
+
+    matte = "1TRQLR9GuNpmGa2CaGEJNlNzHkezDFZpg"
+    fysikk = "19YX0I_o7h5rYkTv_oEWNIepEt7mc7wx7"
+    norsk = "1eYCvh6-chEbWjOVbvsjAtWjCyogco-M5"
+    historie = "1mJrIrr-feyaDjgunud8WkBYW21T2EGib"
+    religion = "1t9Q8LGUfoJlYmYmm8-70jC82yA7S2x4f"
+    felles = "felles"
+    fag = []
+
+    #check pixel rgb value, append blues to list
+    for y in range(h):
+        for x in range(w):
+            r,g,b = pix[x,y]
+            if r in blue[0] and g in blue[1] and b in blue[2]: 
+                # print("BLUE",x,y)
+                fag.append(matte)
+                break
+            elif r in red[0] and g in red[1] and b in red[2]:
+                # print("RED",x,y)
+                fag.append(fysikk)
+                break
+            elif r in green[0] and g in green[1] and b in green[2]:
+                # print("GREEN", x,y)
+                fag.append(norsk)
+                break
+            elif r in yellow[0] and g in yellow[1] and b in yellow[2]:
+                # print("YELLOW",x,y)
+                fag.append(historie)
+                break
+            elif r in pink[0] and g in pink[1] and b in pink[2]:
+                # print("PINK",x,y)
+                fag.append(religion)
+                break
+            else:
+                pass
+    return fag[0]
+
+if __name__ == "__main__":
+    arguments = sys.argv
+    for argument in arguments:
+        if argument == sys.argv[0]:
+            pass
+        else:
+            print(argument, "=", classify(argument))
--- notesync.py
+++ notesync.py
@@ -5,66 +5,13 @@ from oauth2client import file as oauth_file, client, tools
 from apiclient.http import MediaFileUpload
 from apiclient import discovery
 
-from PIL import Image
+import sys
+from identify import classify
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/drive'
 
-# folder ids
-matte = "1TRQLR9GuNpmGa2CaGEJNlNzHkezDFZpg"
-fysikk = "19YX0I_o7h5rYkTv_oEWNIepEt7mc7wx7"
-    
-fag = []
-def classifier():
-    img = Image.open("blue.jpg")
-
-    #resize to smaller image
-    w,h = img.size
-    img.resize((w/10,h//10)).save("tmp.jpg")
-    img.close()
-
-    #open new image
-    img = Image.open("tmp.jpg")
-    pix = img.load
-    
-    w,h = img.size
-
-    blue = [range(60,90), range(110,140), range(110,140)]
-    red = [range(180,230), range(70,115), range(30,70)]
-    green = [range(130,170),range(140,180), range(60,85)]
-    yellow = [range(200,235), range(180,210), range(95,120)]
-    pink = [range(210,240), range(150,180), range(110,130)]
-
-
-    
-    #check pixel rgb value, append blues to list
-    for y in range(h):
-        for x in range(w):
-            r,g,b = pix[x,y]
-            if r in blue[0] and g in blue[1] and b in blue[2]: 
-                print("BLUE",x,y)
-                fag.append(matte)
-                break
-            elif r in red[0] and g in red[1] and b in red[2]:
-                print("RED",x,y)
-                fag.append(fysikk)
-                break
-            elif r in green[0] and g in green[1] and b in green[2]:
-                print("GREEN", x,y)
-                break
-            elif r in yellow[0] and g in yellow[1] and b in yellow[2]:
-                print("YELLOW",x,y)
-                break
-            elif r in pink[0] and g in pink[1] and b in pink[2]:
-                print("PINK",x,y)
-                break
-            else:
-                pass
-        
-classifier()
-print(fag)
-
-def main():
+def main(image):
     """Shows basic usage of the Drive v3 API.
 
     Prints the names and ids of the first 10 files the user has access to.
@@ -76,8 +23,11 @@ def main():
         creds = tools.run_flow(flow, store)
     service = build('drive', 'v3', http=creds.authorize(Http()))
 
+    
+    fag = classify(image)
+
     def uploadFile(filename,filepath,mimetype):
-        folder_id = fag[0]
+        folder_id = fag
         file_metadata = {
             'name': filename,
             'parents': [folder_id]
@@ -90,6 +40,13 @@ def main():
                                         fields='id').execute()
         print('File ID: %s' % file.get('id'))
 
-    uploadFile("blue.jpg","blue.jpg","image/jpeg")
+    uploadFile(image,image,"image/jpeg")
+
 if __name__ == '__main__':
-    main()
+    arguments = sys.argv
+    for argument in arguments:
+        if argument == sys.argv[0]:
+            pass
+        else:
+            main(argument)
+
